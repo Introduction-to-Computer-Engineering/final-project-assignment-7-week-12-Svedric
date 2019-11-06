bb683fc4d5c43ac643fb1c6fc260cb2bbf398ddd
Svedric <lpennell13@gmail.com>
2019-11-06 09:17:18 -0700

    Update twenty_eight.js

--- twenty_eight.js
+++ twenty_eight.js
@@ -4,53 +4,62 @@ pins.analogWritePin(AnalogPin.P2, 0)
 let row = 0
 let column = 0
 let counter = 0
+let wait = 0
+let on = true
 function updateled() {
-    if (row == 4 && column == 4 && counter == 5) {
-        led.unplot(0, 0)
-        led.unplot(0, 1)
-        led.unplot(0, 2)
-        led.unplot(0, 3)
-        led.unplot(0, 4)
-        led.unplot(1, 0)
-        led.unplot(1, 1)
-        led.unplot(1, 2)
-        led.unplot(1, 3)
-        led.unplot(1, 4)
-        led.unplot(2, 0)
-        led.unplot(2, 1)
-        led.unplot(2, 2)
-        led.unplot(2, 3)
-        led.unplot(2, 4)
-        led.unplot(3, 0)
-        led.unplot(3, 1)
-        led.unplot(3, 2)
-        led.unplot(3, 3)
-        led.unplot(3, 4)
-        led.unplot(4, 0)
-        led.unplot(4, 1)
-        led.unplot(4, 2)
-        led.unplot(4, 3)
-        led.unplot(4, 4)
-        row = 0
-        column = 0
-        counter = 0
-    } else {
-        if (counter == 5 - column && row == 4) {
-            counter = 0
-            row = 0
-            column++
-        } else if (counter == 5 - column) {
-            counter = 0
-            row++
+    if (wait == 20) {
+        wait = 0
+        if (on) {
+            if (row == 4 && column == 4 && counter == 1) {
+                on = false
+                row = 0
+                column = 0
+                counter = 0
+            } else {
+                if (counter == 5 - column && row == 4) {
+                    counter = 0
+                    row = 0
+                    column++
+                } else if (counter == 5 - column) {
+                    counter = 0
+                    row++
+                } else {
+                }
+                counter++
+                if (counter > 1) {
+                    led.unplot(row, counter - 2)
+                    led.plot(row, counter - 1)
+                } else if (counter == 1) {
+                    led.plot(row, counter - 1)
+                }
+            }
         } else {
+            if (row == 4 && column == 4 && counter == 1) {
+                on = true
+                row = 0
+                column = 0
+                counter = 0
+            } else {
+                if (counter == 5 - column && row == 4) {
+                    counter = 0
+                    row = 0
+                    column++
+                } else if (counter == 5 - column) {
+                    counter = 0
+                    row++
+                } else {
+                }
+                counter++
+                if (counter > 1) {
+                    led.plot(row, counter - 2)
+                    led.unplot(row, counter - 1)
+                } else if (counter == 1) {
+                    led.unplot(row, counter - 1)
+                }
+            }
         }
-        counter++
-        if (counter > 1) {
-            led.unplot(row, counter - 2)
-            led.plot(row, counter - 1)
-        } else if (counter == 1) {
-            led.plot(row, counter - 1)
-        }
+    } else {
+        wait++
     }
 }
 basic.forever(function () {
@@ -83,4 +92,4 @@ basic.forever(function () {
         updateled()
         control.waitMicros(1000)
     }
-}) 
+})
