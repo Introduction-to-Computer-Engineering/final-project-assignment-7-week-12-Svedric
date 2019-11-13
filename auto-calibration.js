4f1dd47d180c57270cd47bddd34a902611faec31
Svedric <lpennell13@gmail.com>
2019-11-13 09:45:24 -0700

    Update auto-calibration.js

--- auto-calibration.js
+++ auto-calibration.js
@@ -1,14 +1,28 @@
 pins.analogWritePin(AnalogPin.P0, 0)
 pins.analogWritePin(AnalogPin.P1, 0)
 pins.analogWritePin(AnalogPin.P2, 0)
+//Some variables for the screensaver:
 let row = 0
 let column = 0
 let counter = 0
 let wait = 0
 let on = true
+
+//Some variables for the calibration
 let calibrationdone = false
 let lowcalibrated = false
 let highcalibrated = false
+let lowcal1 = 0
+let lowcal2 = 0
+let lowcal3 = 0
+let lowcalavg = 0
+let highcal1 = 0
+let highcal2 = 0
+let highcal3 = 0
+let highcalavg = 0
+let sensorreading = 0 //The moisture sensor input reading
+let displaylevel = 0 //The amount of bars the microbit will display in its led matrix given the input
+
 function unplotall() {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -41,7 +55,7 @@ function unplotall() {
 }
 function calibrate() {
     if (!lowcalibrated) {
-        unplotall()
+        // Display down arrow since lowcalibrated is false
         led.plot(2, 0)
         led.plot(2, 1)
         led.plot(0, 2)
@@ -52,7 +66,7 @@ function calibrate() {
         led.plot(3, 3)
         led.plot(2, 4)
     } else if (!highcalibrated) {
-        unplotall()
+        // Display up arrow since highcalibrated is false
         led.plot(2, 0)
         led.plot(1, 1)
         led.plot(2, 1)
@@ -62,8 +76,65 @@ function calibrate() {
         led.plot(4, 2)
         led.plot(2, 3)
         led.plot(2, 4)
+    } else {
+        calibrationdone = true
+        basic.showString('Calibration success')
     }
 }
+input.onButtonPressed(Button.A, () => {
+    // Button A calibrates low readings
+    if (lowcal1 == 0) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        lowcal1 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    } else if (lowcal2 == 0) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        lowcal2 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    } else if (lowcal3 == 0) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        lowcal3 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        lowcalavg = (lowcal1 + lowcal2 + lowcal3) / 3
+        lowcalibrated = true
+    } else {
+        basic.showString('LOW CALIBRATION RESET')
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        lowcal1 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        lowcal2 = 0
+        lowcal3 = 0
+        lowcalibrated = false
+        calibrationdone = false
+    }
+})
+input.onButtonPressed(Button.B, () => {
+    // Button B calibrates high readings
+    if (highcal1 == 0) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        highcal1 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    } else if (highcal2 == 0) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        highcal2 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    } else if (highcal3 == 0) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        highcal3 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        highcalavg = (highcal1 + highcal2 + highcal3) / 3
+        highcalibrated = true
+    } else {
+        basic.showString('HIGH CALIBRATION RESET')
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        highcal1 = pins.analogReadPin(AnalogPin.P10)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        highcal2 = 0
+        highcal3 = 0
+        highcalibrated = false
+        calibrationdone = false
+    }
+})
 function updateled() {
     if (wait == 20) {
         wait = 0
@@ -150,12 +221,20 @@ basic.forever(function () {
             }
             updateled()
         } else {
+            unplotall()
             if (!calibrationdone) {
                 calibrate()
             } else {
                 pins.digitalWritePin(DigitalPin.P8, 1)
+                sensorreading = pins.analogReadPin(AnalogPin.P10)
+                displaylevel = pins.map(sensorreading, lowcalavg, highcalavg, 0, 4)
+                led.plot(0, 4 - displaylevel)
+                led.plot(1, 4 - displaylevel)
+                led.plot(2, 4 - displaylevel)
+                led.plot(3, 4 - displaylevel)
+                led.plot(4, 4 - displaylevel)
             }
         }
         control.waitMicros(1000)
     }
-}) 
+})
